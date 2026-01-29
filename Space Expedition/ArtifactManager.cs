eb536f50bb380e9f84c2ad3c42511134aca9bd3e
qcw <Cqin9326@gmail.com>
2026-01-28 20:28:17 -0600

    Letter mapping table from assignment

--- Space Expedition/ArtifactManager.cs
+++ Space Expedition/ArtifactManager.cs
@@ -10,5 +10,18 @@ namespace Space_Expedition
     {
         private Artifact[] list = new Artifact[200];
         private int count = 0;
+
+
+        private char[] mapFrom =
+{
+            'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
+
+        private char[] mapTo =
+        {
+            'H','Z','A','U','Y','E','K','G','O','T','I','R','J',
+            'V','W','N','M','F','Q','S','D','B','X','L','C','P'
+        };
     }
 }
