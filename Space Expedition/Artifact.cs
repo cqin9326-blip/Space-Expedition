0b51602be41caefc44df0bdbc9f0bab92ce3b0e4
qcw <Cqin9326@gmail.com>
2026-01-28 20:19:59 -0600

    This class only stores one artifact

--- Space Expedition/Artifact.cs
+++ Space Expedition/Artifact.cs
@@ -8,5 +8,12 @@ namespace Space_Expedition
 {
     internal class Artifact
     {
+        public string EncodedName;
+        public string DecodedName;
+        public string Planet;
+        public string DiscoveryDate;
+        public string StorageLocation;
+        public string Description;
     }
+
 }
