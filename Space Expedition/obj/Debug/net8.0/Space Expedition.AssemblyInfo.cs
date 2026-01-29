c1c46759b736156c619900f08278d540a12d5aa0
qcw <Cqin9326@gmail.com>
2026-01-28 20:16:17 -0600

    initialize all files needed

--- Space Expedition/obj/Debug/net8.0/Space Expedition.AssemblyInfo.cs
+++ Space Expedition/obj/Debug/net8.0/Space Expedition.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Space Expedition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bccdf2565e395c4c4fe5563edeca1b156140e667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Space Expedition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Space Expedition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
